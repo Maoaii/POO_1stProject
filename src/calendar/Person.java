b23e21731d103472ce00a9a81ae2658582be8960
Maoaii <65407819+Maoaii@users.noreply.github.com>
2022-04-14 19:33:59 +0100

    Uploaded version 1

---
+++ src/calendar/Person.java
@@ -0,0 +1,22 @@
+package calendar;
+
+public interface Person {
+	
+	/**
+	 * @return the name of this person
+	 */
+	public String getName();
+	
+	/**
+	 * To be implemented in <code>StudentClass</code>
+	 * @return the id of this student
+	 */
+	abstract public String getId();
+	
+	/**
+	 * @return the number of courses this person is registered in
+	 */
+	public int getNumCourses();
+	
+	
+}
