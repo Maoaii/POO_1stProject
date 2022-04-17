daf0618f0832e5047de5d227f242e63db99af045
Maoaii <65407819+Maoaii@users.noreply.github.com>
2022-04-17 15:35:03 +0100

    Completed EvalCalendar Interface

--- src/calendar/EvalCalendar.java
+++ src/calendar/EvalCalendar.java
@@ -109,4 +109,225 @@ public interface EvalCalendar {
 	 * @pre courseName != null && isCourseRegistered(courseName)
 	 */
 	public void listRoster(String courseName);
+	
+	
+	/**
+	 * 2.9 <code>assign</code> COMMAND
+	 */
+	
+	/**
+	 * Assign a new professor to a course
+	 * 
+	 * @param name
+	 * @param courseName
+	 * @pre name != null && courseName != null
+	 */
+	public void assignProfessor(String name, String courseName);
+
+	/**
+	 * 
+	 * @param name
+	 * @param courseName
+	 * @pre name != null && courseName != null
+	 * @return true if professor with <code>name</code> is assigned to course with <code>courseName</code>
+	 */
+	public boolean isProfessorAssigned(String name, String courseName);
+
+
+	/**
+	 * 2.10 enrol COMMAND
+	 */
+
+	/**
+	 * Enrolls students to a course
+	 * 
+	 * @param numStudents
+	 * @param courseName
+	 * @param studentNames
+	 * @pre courseName != null && studentNames != null
+	 */
+	public void enrolStudents(int numStudents, String courseName, String[] studentNames);
+
+	/**
+	 * Checks if there's atleast one student to enrol in a course
+	 * 
+	 * @param numStudents
+	 * @return true if <code>numStudents > 0</code>
+	 */
+	public boolean atleastOneEnrol(int numStudents);
+
+	/**
+	 * Checks if student is enroled in class
+	 * 
+	 * @param name
+	 * @param courseName
+	 * @return true if student with <code>name</code> is enroled in course with <code>courseName</code>
+	 */
+	public boolean isStudentEnroled(String name, String courseName);
+
+	
+	/**
+	 * 2.11 intersection COMMAND
+	 */
+	
+	/**
+	 * Lists all people that are enroled in all courses given
+	 * 
+	 * @param numCourses
+	 * @param courseNames
+	 * @pre courseNames != null
+	 */
+	public void listPeopleIntersection(int numCourses, String[] courseNames);
+
+	/**
+	 * Checks if there's atleast two courses to intersect with
+	 * 
+	 * @param numCourses
+	 * @return true if <code>numCourses >= 2</code>
+	 */
+	public boolean atleastTwoCourses(int numCourses);
+
+	
+	/**
+	 * 2.12 coursedeadlines COMMAND
+	 */
+	
+	/**
+	 * Lists the course deadlines
+	 * 
+	 * @param courseName
+	 * @pre courseName != null
+	 */
+	public void listCourseDeadlines(String courseName);
+	
+	
+	/**
+	 * 2.13 personaldeadlines COMMAND
+	 */
+	
+	/**
+	 * Lists the person's deadlines
+	 * 
+	 * @param name
+	 * @pre name != null
+	 */
+	public void listPersonalDeadlines(String name);
+
+	
+	/**
+	 * 2.14 deadline COMMAND
+	 */
+	
+	/**
+	 * Adds a new deadline to a course
+	 * 
+	 * @param courseName
+	 * @param date
+	 * @param deadlineName
+	 * @pre courseName != null && date != null && deadlineName != null
+	 */
+	public void addDeadline(String courseName, int year, int month, int day, String deadlineName);
+
+	/**
+	 * Checks if course with <code>courseName</code> has deadline with <code>deadlineName</code>
+	 * 
+	 * @param deadlineName
+	 * @pre deadlineName != null
+	 * @return true if course with <code>courseName</code> has deadline with <code>dealineName</code> 
+	 */
+	public boolean doesCourseHaveDealine(String courseName, String deadlineName);
+
+
+	/**
+	 * 2.15 coursetests COMMAND
+	 */
+	
+	/**
+	 * Lists course (with <code>courseName</code>) tests
+	 * 
+	 * @param courseName
+	 * @pre courseName != null
+	 */
+	public void listCourseTests(String courseName);
+	
+
+	/**
+	 * 2.16 personaltests COMMAND
+	 */
+	
+	/**
+	 * Lists student (with <code>name</code>) tests
+	 * 
+	 * @param name
+	 * @pre name != null
+	 */
+	public void listStudentTests(String name);
+	
+	
+	/**
+	 * 2.17 schedule COMMAND
+	 */
+	
+	/**
+	 * Schedules a new test for course with <code>courseName</code>
+	 * 
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @param hour
+	 * @param duration
+	 * @param courseName
+	 * @param testName
+	 * @pre courseName != null && testName != null
+	 */
+	public void scheduleTest(int year, int month, int day, int hour, int duration, String courseName, String testName);
+	
+	/**
+	 * Checks if course with <code>courseName</code> has a test with <code>testName</code>
+	 * 
+	 * @param courseName
+	 * @param testName
+	 * @pre courseName != null && testName != null
+	 * @return true if course has a test with <code>testName</code>
+	 */
+	public boolean isTestNameTaken(String courseName, String testName);
+
+	/**
+	 * Checks if test with <code>testName</code> has conflicting time with another one
+	 * 
+	 * @param testName
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @param hour
+	 * @param duration
+	 * @pre testName != null
+	 * @return true if test has conflicting times
+	 */
+	public boolean isTestTimeTaken(String testName, int year, int month, int day, int hour, int duration);
+	
+	
+	/**
+	 * 2.18 superprofessor COMMAND
+	 */
+	
+	/**
+	 * Gets the professor with the most students
+	 * 
+	 * @return the name of the professor
+	 */
+	public String getSuperProfessor();
+	
+	
+	/**
+	 * 2.19 stressometer COMMAND
+	 */
+	
+	/**
+	 * Lists the top <code>nStudents</code> stressed students.
+	 * A student is stressed based on the amount of tests in one week
+	 * 
+	 * @param nStudents
+	 */
+	public void listTopNStressedStudents(int nStudents);
 }
