dd4647bbc86d2e36985d45e1d2f80a423586527f
Maoaii <65407819+Maoaii@users.noreply.github.com>
2022-04-17 19:38:04 +0100

    Did Command enum

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
 
 public class Main {
+	
+	// Constants
+		// Commands
+	private enum Command {
+		EXIT, HELP, PEOPLE, PROFESSOR, STUDENT, COURSES, COURSE, ROSTER, ASSIGN, ENROL, INTERSECTION,
+		COURSEDEADLINES, PERSONALDEADLINES, DEADLINE, COURSETESTS, PERSONALTESTS, SCHEDULE, SUPERPROFESSOR,
+		STRESSOMETER
+	};
+	
+		// Output messages
+	private static final String UNKNOWN_COMMAND = "Unknown command %s. Type help to see available commands.\n";
+	private static final String EXIT_MESSAGE = "Bye!";
+	private static final String HELP_MESSAGE = "Available commands:";
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
